0f62d6df09a45ab8d16e526839a2d9442971787d
lewys-miugo <lewiswambugu01@gmail.com>
2025-07-19 23:10:41 +0300

    Creating the footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="App-footer">
+      <p>&copy; 2024 Smart Goal Planner. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
