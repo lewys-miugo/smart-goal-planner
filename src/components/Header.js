3fd50e33dbf14f69007770e8128c63e453850b88
lewys-miugo <lewiswambugu01@gmail.com>
2025-07-21 10:25:48 +0300

    Adding github link

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="App-header">
-      <h1>Smart Goal Planner</h1>
+      <h1>Smart Goal Planner</h1> <span><a href="https://github.com/lewys-miugo/smart-goal-planner">Github Link</a></span>
     </header>
   );
 };
