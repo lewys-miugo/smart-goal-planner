054842980f4c3be9cdf6246d2565f28a8be5873d
lewys-miugo <lewiswambugu01@gmail.com>
2025-07-22 08:28:13 +0300

    Adding valid github link

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="App-header">
-      <h1>Smart Goal Planner</h1> <span><a href="https://github.com/lewys-miugo/smart-goal-planner">Github Link</a></span>
+      <h1>Smart Goal Planner</h1> <a href="https://github.com/lewys-miugo/smart-goal-planner">Github Link</a>
     </header>
   );
 };
